ba7e01f8370355d2155ca533384dc8cbe57816a7
sinyahmon <105526437+sinyahmon@users.noreply.github.com>
2022-05-15 13:24:45 -0400

    HelloWorld changes for CIS 1500
    Lab 1 completed

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -2,6 +2,10 @@ package helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Question 1) Introducing Rob Waldrop. Rob is majoring in CIS - information technology support. He is completing this CIS 1500 introduction to java class as they are core requirements for his degree.");
+        System.out.println("Question 2) Rob has previous programming experience in SQL for building reports and small amounts of Microsoft Access VBA.");
+        System.out.println("Question 3) As far as hobbies, Rob is interested in cornhole, reading, and lifting weights.");
+        System.out.println("Question 4) Rob's plans for this summer is just work and school.");
+        System.out.println("Question 5) Rob does not have anything else to share to the class.");
     }
 }
