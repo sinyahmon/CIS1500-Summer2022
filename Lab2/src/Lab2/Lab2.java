123855f762c31a5cf5ecbbafbba789c5bd6bc826
sinyahmon <105526437+sinyahmon@users.noreply.github.com>
2022-05-18 21:49:53 -0400

    Lab 2 updates
    lab 2 logic implemented. need to add input from user.

--- Lab2/src/Lab2/Lab2.java
+++ Lab2/src/Lab2/Lab2.java
@@ -2,9 +2,14 @@ package Lab2;
 
 public class Lab2 {
     public static void main(String[] args) {
-        double userHourlyWage;
-        int userHoursWorked;
+        double userHourlyWage = 20.00;
+        int userHoursWorked = 40;
 
-
+        double userGrossPay = userHourlyWage * userHoursWorked;
+        System.out.println(userGrossPay);
+        double userTaxes = userGrossPay * 0.10;
+        System.out.println(userTaxes);
+        double userNetPay = userGrossPay - userTaxes;
+        System.out.println(userNetPay);
     }
 }
