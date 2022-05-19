ab09f44401b496419efe2a04361abd23b5d06fca
sinyahmon <105526437+sinyahmon@users.noreply.github.com>
2022-05-19 17:23:32 -0400

    Lab 2 updates
    Finished. Review later.

--- Lab2/src/Lab2/Lab2.java
+++ Lab2/src/Lab2/Lab2.java
@@ -1,15 +1,19 @@
 package Lab2;
-
+import java.util.Scanner;
 public class Lab2 {
     public static void main(String[] args) {
-        double userHourlyWage = 20.00;
-        int userHoursWorked = 40;
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Please enter your hourly wage: ");
+        double userHourlyWage = Double.parseDouble(keyboard.nextLine());
+        System.out.println("Please enter your hours worked this week: ");
+        int userHoursWorked = Integer.parseInt(keyboard.nextLine());
 
         double userGrossPay = userHourlyWage * userHoursWorked;
-        System.out.println(userGrossPay);
+        System.out.println("Your gross pay is: " + userGrossPay);
         double userTaxes = userGrossPay * 0.10;
-        System.out.println(userTaxes);
+        System.out.println("Your taxes owed is: " + userTaxes);
         double userNetPay = userGrossPay - userTaxes;
-        System.out.println(userNetPay);
+        System.out.println("Your net pay is: " + userNetPay);
     }
 }
