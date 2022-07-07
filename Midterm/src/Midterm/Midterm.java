2725c0cf751c9aad066d66469e4192de317faadd
sinyahmon <105526437+sinyahmon@users.noreply.github.com>
2022-07-07 12:08:51 -0400

    Midterm Updates

---
+++ Midterm/src/Midterm/Midterm.java
@@ -0,0 +1,40 @@
+package Midterm;
+
+import java.util.Scanner;
+
+public class Midterm {
+    public static void main(String[] args) {
+        /*
+        Write a java program that asks the user to enter the total amount of money they have to spend,
+        then in a loop, ask them to enter the price of an item they are buying.  If they have enough money to
+        spend, tell them they bought the item and display the new balance of money they have left to spend.
+        If they do not have enough money to spend, tell them they can't buy that item,
+        and display the remaining total amount of money they have to spend.
+        Allow them to continue purchasing items until they want to be done.
+        */
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("What is the total amount of money you have to spend? :");
+    Double totalAmt = Double.parseDouble(keyboard.nextLine());
+    String continueLoop = "";
+    while (continueLoop.equalsIgnoreCase("n")) {
+        System.out.println("What is the price of the item you are buying? :");
+        double priceOfItem = Double.parseDouble(keyboard.nextLine());
+        if (priceOfItem < totalAmt) {
+            totalAmt = totalAmt - priceOfItem;
+            System.out.println("Your new balance is: " + totalAmt);
+            continue;
+        } else if (priceOfItem > totalAmt) {
+            System.out.println("You don't have enough money to buy that item.");
+            System.out.println("Your balance is:: " + totalAmt);
+            continue;
+        } else {
+            System.out.println("Error.");
+        }
+        System.out.println("Would you like to continue? (Y/N): ");
+        continueLoop = keyboard.nextLine();
+        if (continueLoop.equalsIgnoreCase("n")) {
+            System.out.println("Thank you for your business!");
+        }
+    }
+}
+}
